9bab5fc99b2e0eb34a5f2236887258602d2097da
nemo <60929499+youyou1cha@users.noreply.github.com>
2022-08-11 14:43:24 +0800

    下班

---
+++ text7.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# format
+
+x = 1234.5678901
+
+print(format(x,'0.2f'))
+print(format(x,'>10.1f'))
+print(format(x,'^10.3f'))
+print(format(x,','))
+print(format(x,'0,.1f'))
---
+++ time1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 十进制 转 二进制 八进制 十六进制 bin oct hex
+# format b 0 x
+# 进制转换 int 整数转换
+
+# 字节到整数的转换 byte int
+# Int.from_bytes to_bytes
+
+# 复数 complex 3-5j
+
+# 无穷大 float('inf') float('-inf') float('nan') 测试math.isinf() math.isnan()
+
+# 随机选择
+import random
+random.choice()
+random.sample()
+random.randint()
+random.Random()
+random.getrandbits()
+random.seed()
